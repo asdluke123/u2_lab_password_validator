59436c57ad67557277cf86d3614f27d8375f559c
asdluke123 <asdluke1234@gmail.com>
2022-07-13 23:14:49 +0200

    complete hw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import Validator from "./components/Validator"
-
+import { useState } from "react"
 const App = () => {
+const [username,setUsername] = useState('')
+const [password,setPassword] = useState('')
+const [passwordConfirm,setPasswordConfirm] = useState('')
+const [valid,setValid] = useState(null)
+const [validText,setValidText] = useState('')
+const handleChange = (e)=>{
+  if(e.target.id === 'username'){
+    setUsername(e.target.value)
+  }
+  else if(e.target.id === 'password'){
+    setPassword(e.target.value)
+  }
+  else if(e.target.id === 'passwordConfirm'){
+    setPasswordConfirm(e.target.value)
+  }
+}
+const submitForm = (e) =>{
+  e.preventDefault();
+  if(password.length>7 && passwordConfirm.length>7){
+    if(password === passwordConfirm){
+    setValid('valid')
+    setValidText('Thank you, you are all signed up!')}
+  }else{
+    setValid('invalid')
+    setValidText('Please match you password,try again')
+  }
+}
 
+const cancelForm = () =>{
+  setPassword('')
+  setUsername('')
+  setPasswordConfirm('')
+}
   return (
-    <Validator />
+    <Validator username={username} password={password} passwordConfirm={passwordConfirm} validText={validText} valid={valid} handleChange={handleChange} submitForm={submitForm} cancelForm = {cancelForm}/>
   )
 }
 
--- src/components/Validator.jsx
+++ src/components/Validator.jsx
@@ -1,27 +1,25 @@
 import '../styles/Validator.css'
 
-const Validator = () => {
+const Validator = (props) => {
   
   return (
     <div className="form">
       <h1>Sign Up</h1>
       <form>
-        <input type="text" placeholder="Username" id="username" />
+        <input onChange = {props.handleChange} value={props.username} type="text" placeholder="Username" id="username" />
         <label htmlFor="username">Username</label>
 
-        <input type="password" placeholder="Password" id="password" />
+        <input onChange = {props.handleChange} value={props.password}type="password" placeholder="Password" id="password" />
         <label htmlFor="password">Password</label>
 
-        <input
-          type="password"
-          placeholder="Confirm password"
-          id="passwordConfirm"
-        />
+        <input value={props.passwordConfirm} onChange = {props.handleChange} type="password" placeholder="Confirm password" id="passwordConfirm"/>
         <label htmlFor="passwordConfirm">Confirm password</label>
-
-        <button type="submit">Sign Up</button>
-        <p>Passwords must match.</p>
+        
+        <button type = "submit" onClick={props.submitForm}>Sign Up</button>
+        <button type = 'button' onClick={props.cancelForm}class = 'cancel'>Reset</button>
+        <p className = {props.valid}>Passwords must match.</p>  
       </form>
+  
     </div>
   )
 }
